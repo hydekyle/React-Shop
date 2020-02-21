23c733d0e1b2a62e965a9975c4e5605bd73da77b
Hyde_Wick <hydekyle@hotmail.com>
2020-02-21 01:00:33 +0000

    added some things

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import Login from  './components/login/Login.tsx';
 import Menu from './components/menu/Menu';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <i class="fas fa-home" />
       <Menu />
       <div className="Body"><p>Aquí va el body-content</p></div>
+      <i class="fab fa-font-awesome"></i>
       <Login />
     </div>
   );
