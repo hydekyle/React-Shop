95224c9ed719ac6dad107540a0726d8b2507374e
Hyde_Wick <hydekyle@hotmail.com>
2020-02-26 16:17:43 +0000

    fixing things

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const morgan = require("morgan")
+const compression = require("compression")
+const cors = require("cors");
+const app = express();
+const corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(morgan("combined"));
+app.use(compression());
+
+
+app.listen(8080, () => console.log("Ready on port 8080!"));
