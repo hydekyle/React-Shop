6fc82c21c3e2d73ee4d36476ddfadd31f3e5a4e2
Hyde_Wick <hydekyle@hotmail.com>
2020-03-04 22:24:18 +0000

    playing around

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Login from  './components/login/Login'
 import Menu from './components/menu/Menu'
-import Articles from './components/articles/Articles'
 import ItemCard from './components/item-card/ItemCard'
 import BoxInput from './components/box-input/BoxInput'
-import {MainProvider} from './Context/MainContext'
+import BannerWelcome from './components/banner-welcome/BannerWelcome'
+import { MainProvider } from './Context/MainContext'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
@@ -14,10 +14,10 @@ export default () =>
       <MainProvider>
         <Menu/>
         <BoxInput/>
-        <ItemCard/>
+        <BannerWelcome/>
           <Switch>
             <Route path='/login' component={Login}></Route>
-            <Route path='/shop' component={Articles}></Route>
+            <Route path='/shop' component={ItemCard}></Route>
           </Switch>
         </MainProvider>
     </div>
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,15 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import {MainContext} from '../../Context/MainContext'
-import { Button, CardBody} from 'reactstrap'
+import { 
+    Button, 
+    CardBody,
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    Collapse,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap'
+
 export default () => {
+    const [isOpen, setIsOpen] = useState(false)
     const context = useContext(MainContext)
     return (
         <div>
-            <Button 
-                color="danger"
-                onClick={() => context?.resetCounter()}
-                >Danger!!</Button>
-            <CardBody>{context?.counter}</CardBody>
+            <Navbar dark={true}>
+            <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+            <NavbarBrand href="/">Tazas Locas</NavbarBrand>
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/login">Login</NavLink>
+                        <NavLink href="/shop">Shop</NavLink>
+                    </NavItem>
+                </Nav>
+                <CardBody>{context?.counter}</CardBody>
+            </Collapse>
+            </Navbar>
+            
         </div>
     )
 }
