f36b3ac151b427fabeea4d21a4d89f94931b42ac
Hyde_Wick <hydekyle@hotmail.com>
2020-03-05 18:27:50 +0000

    moving things

--- src/api/products/controller.ts
+++ src/api/products/controller.ts
@@ -1,12 +1,21 @@
 const myApi = require("../apis")
+const regOnlyCharsAndNumbers = new RegExp('[^A-z|0-9]')
 
 export const get_my_advice = async function() {
     const data = await myApi.get_advice()
     return data
 }
 
-export const get_user = async (name : String) => {
-    let data = await myApi.get_user(name)
-    if (!data) data = "Usuario no encontrado"
-    return data
+export const get_user = async (name : string) => {
+    if (isValidName(name)) {
+        let data = await myApi.get_user(name)
+        if (!data) data = "Usuario no encontrado"
+        return data
+    } else {
+        return "Please, provide a valid username"
+    }
+}
+
+const isValidName = (name : string) => {
+    return !regOnlyCharsAndNumbers.test(name)
 }
--- src/api/products/index.ts
+++ src/api/products/index.ts
@@ -1,16 +1,12 @@
 const controller = require("./controller")
 import * as express from 'express'
 const router = express.Router()
-const regOnlyCharsAndNumbers = new RegExp('[^A-z|0-9]')
+
 
 router.get("/users", (req, res) => {
     const name = req.query["name"]
-    if (name !== undefined && !regOnlyCharsAndNumbers.test(name)) {
-        controller.get_user(name)
-            .then(val => res.send(val))
-    } else {
-        res.send("Please, provide a valid username")
-    }
+    controller.get_user(name)
+        .then(val => res.send(val))
 })
 
 router.get("/", (req, res) => {
