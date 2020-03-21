5df28068007ed432eda8e84e193ab3cf838c2451
Hyde_Wick <hydekyle@hotmail.com>
2020-03-21 23:32:29 +0000

    full moon

--- src/components/game-moons/GameMoons.tsx
+++ src/components/game-moons/GameMoons.tsx
@@ -2,84 +2,81 @@ import React, { useState } from 'react'
 import './GameMoons.css'
 import * as _ from 'lodash'
 
-
 interface Props {
     rows: number
     columns: number
     pointer: number
+    map: Map<String, number>
 }
 
 export default () => {
-
-    const [props, setProps] = useState<Props>({
-        rows: 3,
-        columns: 3,
-        pointer: 0
-    })
 
     const moons = ["🌕", "🌖", "🌗", "🌘", "🌑", "🌒", "🌓", "🌔"]
     const minRows = 1, minColumns = 1
     const maxRows = 100, maxColumns = 100
 
-    const Rotate = (wheelEvent: React.WheelEvent) => {
-        if (wheelEvent.deltaY < 0) RotateLeft()
-        else RotateRight()
-    }
+    const [props, setProps] = useState<Props>({
+        rows: 3,
+        columns: 3,
+        pointer: 0,
+        map: new Map()
+    })
 
-    const RotateRight = () => {
-        setProps({
-            ...props,
-            pointer: props.pointer < moons.length - 1 ? props.pointer + 1 : 0
-        })
-    }
-
-    const RotateLeft = () => {
-        setProps({
-            ...props,
-            pointer: props.pointer > 0 ? props.pointer - 1 : moons.length - 1
-        })
-    }
-
-    const ChangeRows = value => {
+    const SetRows = (value: number) => {
         setProps({
             ...props,
             rows: _.clamp(props.rows + value, minRows, maxRows)
         })
     }
 
-    const ChangeColumns = value => {
+    const SetColumns = (value: number) => {
         setProps({
             ...props,
             columns: _.clamp(props.columns + value, minColumns, maxColumns)
         })
     }
 
-    const Canta = value => {
-        console.log(value)
+    const RotateMoon = (moonID: string, wheelEvent: React.WheelEvent) => {
+        let newMoonValue = props.map[moonID]
+        if (wheelEvent.deltaY < 0) {
+            newMoonValue = newMoonValue > 0 ? newMoonValue - 1 : moons.length - 1
+        }
+        else {
+            newMoonValue = newMoonValue < moons.length - 1 ? newMoonValue + 1 : 0
+        }
+        SetMoonValue(moonID, newMoonValue)
+    }
+
+    const SetMoonValue = (ID: string, newValue: number) => {
+        let newMap = props.map
+        newMap[ID] = newValue
+        setProps({
+            ...props,
+            map: newMap
+        })
     }
 
     return (
         <div>
             <div className='box'>
-                <button onClick={() => ChangeRows(-1)}>Row-</button>
-                <button onClick={() => ChangeRows(1)}>Row+</button>
-                <button onClick={() => ChangeColumns(-1)}>Column-</button>
-                <button onClick={() => ChangeColumns(1)}>Column+</button>
+                <button onClick={() => SetRows(-1)}>Row-</button>
+                <button onClick={() => SetRows(1)}>Row+</button>
+                <button onClick={() => SetColumns(-1)}>Column-</button>
+                <button onClick={() => SetColumns(1)}>Column+</button>
             </div>
             <div className='box'>
                 {Array.from(Array(props.rows), (e, rowNumber) => {
                     return <div
                         className='moon-picker'
-                        key={rowNumber}
-                        onWheel={(wheelEvent) => Rotate(wheelEvent)}>
+                        key={rowNumber}>
 
                         {
                             Array.from(Array(props.columns), (e, columnNumber) => {
                                 const myKey = `${rowNumber}|${columnNumber}`
                                 return <div
                                     key={myKey}
-                                    onClick={() => { Canta(myKey) }}
-                                >{moons[props.pointer]}</div>
+                                    onWheel={wheelEvent => RotateMoon(myKey, wheelEvent)}
+                                >{moons[props.map[myKey] === undefined ? 0 : props.map[myKey]]}</div>
                             })
                         }
 
