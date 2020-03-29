50dd55400b6ec4f88855ab6c7009ef7420f66683
Hyde_Wick <hydekyle@hotmail.com>
2020-03-30 00:47:55 +0100

    testing twitter api

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,37 @@
+import * as Twitter from "twitter"
+
 const products_router = require("./api/products/index")
 const express = require("express")
 const morgan = require("morgan")
 const compression = require("compression")
 const cors = require("cors")
 
-const app = express();
+const app = express()
 const corsOptions = {
   origin: "http://localhost:3000"
 };
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(morgan("combined"));
-app.use(compression());
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(morgan("combined"))
+app.use(compression())
 
 app.use("/db", products_router)
 
-app.listen(8080, () => console.log("Ready on port 8080!"));
+//Twitter Bot
+const config = require("./twitter-config")
+let twitter: Twitter = new Twitter(config)
+
+app.get("/twita", (req, res) => {
+  console.log("Working " + twitter.VERSION)
+  twitter.get('friends/ids', { screen_name: config.screen_name, count: 100 }, function (err, data) {
+    if (!err && data) {
+      res.render('index', { friends: data })
+    }
+    else {
+      res.send("Wrong shit: " + err.message)
+    }
+  });
+})
+
+app.listen(8080, () => console.log("Ready on port 8080!"))
