f3c7235777d08468a7c82a5dc883201c314d9390
Hyde_Wick <hydekyle@hotmail.com>
2020-04-10 08:46:48 +0100

    working

--- src/server.ts
+++ src/server.ts
@@ -22,17 +22,20 @@ app.use("/db", products_router)
 
 //Twitter Bot
 const config = {
-  replyTweet: false, //Careful spam
+  countries_filter: ["us", "usa", "united states", "new york", "america"],
   countryFilter: false,
+  paid_link: "https://youtu.be/Rd2m3-gJ1zU",
+  replyTweet: true, //Careful spam
+  followTweetOwner: false,
+  muteAfterFollow: false,
   showTweet: true,
-  followTweetOwner: true,
-  muteAfterFollow: true,
-  followIntervalMinutes: 1
+  followIntervalMinutes: 1,
+  replyIntervalMinutes: 1
 }
 
 const tweets_filter = "#sub4sub"
 const getReplyText = receiverName => {
-  return `@${receiverName} Ser pambisito da mucho askito, pasalo.`
+  return `@${receiverName} Free and fast! ${config.paid_link}`
 }
 
 const GetKeys = () => {
@@ -45,23 +48,20 @@ const GetKeys = () => {
   return result
 }
 
-const countries_filter = ["us", "usa", "united states", "new york", "america"]
-const paid_link = "https://medium.com/@ayozefernndezacosta/t%C3%ADtulo-de-prueba-cbcd6eb57da0"
-
-
 const twitter_keys = GetKeys()
 let twitter: Twitter = new Twitter(twitter_keys)
 let counter_accounts = 0
-let saved_accounts: Array<string> = []
+let saved_accounts: Array<TweetData> = []
+
+interface TweetData {
+  screen_name: string,
+  tweetID: number
+}
 
 twitter.stream("statuses/filter", { track: tweets_filter }, stream => {
 
   stream.on("data", tweet => {
-    if (checkCountry(tweet)) {
-      if (config.showTweet) showTweet(tweet)
-      if (config.followTweetOwner) saveAccount(tweet.user.screen_name)
-      if (config.replyTweet) toReply(tweet.id_str, tweet.user.screen_name)
-    }
+    if (checkCountry(tweet)) handleIncomingTweet(tweet)
   })
 
   stream.on("error", error => {
@@ -69,6 +69,14 @@ twitter.stream("statuses/filter", { track: tweets_filter }, stream => {
   })
 
 })
+
+const handleIncomingTweet = tweet => {
+  if (config.showTweet) showTweet(tweet)
+  saveLastTweet({
+    screen_name: tweet.user.screen_name,
+    tweetID: tweet.id_str
+  })
+}
 
 const checkCountry = tweet => {
   if (!config.countryFilter) return true
@@ -78,30 +86,14 @@ const checkCountry = tweet => {
   }
   let countries = tweet.user.location.split(",")
   for (let country of countries)
-    for (let word of countries_filter)
-      if (word == country.trim().toLowerCase()) return true
+    for (let filter of config.countries_filter)
+      if (filter == country.trim().toLowerCase()) return true
   return false
 }
 
 const showTweet = tweet => {
   console.log(tweet.text)
   if (tweet.user.location) console.log(`lang: ${tweet.lang} | county_code: ${tweet.user.location}`)
-}
-
-const getLastFollowers = maxNumber => {
-  twitter.get("followers/list", { count: maxNumber, skip_status: true }, (error, response) => {
-    if (error) console.warn(error)
-    else {
-      console.log(response.users)
-    }
-  })
-}
-
-const toFollow = screenName => {
-  twitter.post("friendships/create", { screen_name: screenName }, (error, response) => {
-    if (error) console.warn(error)
-    else if (config.muteAfterFollow) toMute(screenName)
-  })
 }
 
 const toMute = screenName => {
@@ -111,19 +103,26 @@ const toMute = screenName => {
   })
 }
 
-const toReply = (tweetID: string, tweetOwnerName: string) => {
-  let replyText = getReplyText(tweetOwnerName)
-  twitter.post("statuses/update", { status: replyText, in_reply_to_status_id: tweetID }, (error, response) => {
-    if (!error) console.log("He respondido a " + tweetOwnerName)
+const toReply = (tweetData: TweetData) => {
+  let replyText = getReplyText(tweetData.screen_name)
+  twitter.post("statuses/update", { status: replyText, in_reply_to_status_id: tweetData.tweetID }, (error, response) => {
+    if (!error) console.log("He respondido a " + tweetData.screen_name)
     else console.warn(error)
   })
 }
 
-const saveAccount = accountScreenName => {
-  saved_accounts.push(accountScreenName)
+const toFollow = (tweetData: TweetData) => {
+  twitter.post("friendships/create", { screen_name: tweetData.screen_name }, (error, response) => {
+    if (error) console.warn(error)
+    else if (config.muteAfterFollow) toMute(tweetData.screen_name)
+  })
 }
 
-let followLastFollower = () => {
+const saveLastTweet = (tweetData: TweetData) => {
+  saved_accounts.push(tweetData)
+}
+
+let followLastSaved = () => {
   if (saved_accounts.length > 0) {
     toFollow(saved_accounts.pop())
     counter_accounts++
@@ -131,11 +130,30 @@ let followLastFollower = () => {
   }
 }
 
+let replyLastSaved = () => {
+  if (saved_accounts.length > 0) {
+    toReply(saved_accounts.pop())
+    counter_accounts++
+    console.log(`Replies: ${saved_accounts.length}`)
+  }
+}
+
+const start = () => {
+  if (config.replyTweet) {
+    setInterval(() => {
+      replyLastSaved()
+    }, 60000 * config.followIntervalMinutes)
+  }
+  if (config.followTweetOwner) {
+    setInterval(() => {
+      followLastSaved()
+    }, 60000 * config.followIntervalMinutes)
+  }
+}
+
 app.listen(PORT, () => {
   console.log(`Ready on port ${PORT}!`)
   console.log(`Starting ${twitter_keys.consumer_key}. Fetching data now.`)
-  setInterval(() => {
-    followLastFollower()
-  }, 60000 * config.followIntervalMinutes)
+  start()
 })
 
