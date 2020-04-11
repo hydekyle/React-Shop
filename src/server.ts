ee84e6348b16d10d11f8ccee7aaadd9af56fb548
Hyde_Wick <hydekyle@hotmail.com>
2020-04-11 09:45:33 +0100

    wp

--- src/server.ts
+++ src/server.ts
@@ -40,7 +40,7 @@ const config = {
   muteAfterFollow: false,
   showTweet: true,
   followIntervalMinutes: 1,
-  replyIntervalMinutes: 5, //Perfect fit for 300 tweets/3h API LIMIT
+  replyIntervalMinutes: 0.66, //Perfect fit for 300 tweets/3h API LIMIT
   replyTweet: true, //Careful spam
 }
 
@@ -63,11 +63,14 @@ const getReplyText = receiverName => {
 
 const getRandomPambi = () => {
   let pambi: string = ""
-  switch (_.random(1, 7)) {
+  switch (_.random(0, 9)) {
     case 1: pambi = "pambisimio"; break
     case 2: pambi = "pambiretrasado"; break
     case 3: pambi = "pambidiota"; break
     case 4: pambi = "pambimierder"; break
+    case 5: pambi = "pambiaskeroso"; break
+    case 6: pambi = "pambisidoso"; break
+    case 7: pambi = "pambicanceroso"; break
     default: pambi = "pambisito"
   }
   return pambi
@@ -75,15 +78,23 @@ const getRandomPambi = () => {
 
 const getRandomInsult = () => {
   let phrase: string = ""
-  switch (_.random(0, 8)) {
-    case 1: phrase = `Si te hacen bulling en el cole tienes más papeletas para ser ${getRandomPambi()}.`; break
-    case 2: phrase = `Dalas tiene un verdadero problema mental y su contenido siempre es basura tóxica. Si te gusta eso, eso es lo que eres.`; break
+  const pambiInsult = getRandomPambi()
+  switch (_.random(0, 15)) {
+    case 1: phrase = `Si te hacen bulling en el cole tienes más papeletas para ser ${pambiInsult}.`; break
+    case 2: phrase = `¡Vamos ${pambiInsult}s, todos en manada a pagarle los juicios a Dalas para que tengáis más contenido tóxico!`; break
     case 3: phrase = `A Dalas solo le apoyan niños sin amigos y cuentas fan penosas, planteate tú por qué.`; break
-    case 4: phrase = `Debe ser duro ser ${getRandomPambi()}. Bullying en el cole, bullying en Twitter...`; break
-    case 5: phrase = `A los ${getRandomPambi()}s hay que echarles de comer a parte.`; break
-    case 6: phrase = `Qué asko me dan los ${getRandomPambi()}s`; break
-    case 7: phrase = `He llegado a la conclusión que a los ${getRandomPambi()} les gusta que les insulten.`; break
-    default: phrase = `Dalas da mucho asko, pero los ${getRandomPambi()}s dan aún más askete.`
+    case 4: phrase = `Debe ser duro ser ${pambiInsult}. Bullying en el cole, bullying en Twitter...`; break
+    case 5: phrase = `A los ${pambiInsult}s hay que echarles de comer a parte.`; break
+    case 6: phrase = `Qué asko me dan los ${pambiInsult}s`; break
+    case 7: phrase = `He llegado a la conclusión que a los ${pambiInsult}s les gusta que les insulten.`; break
+    case 8: phrase = `Los ${pambiInsult}s me dan ganas de vomitar.`; break
+    case 9: phrase = `Los ${pambiInsult}s me dan ganas de vomitar. MUCHAS ganas de vomitar.`; break
+    case 10: phrase = `Los ${pambiInsult}s son muy tristes, me dan mucha penita.`; break
+    case 11: phrase = `¿Cuán triste tiene que ser la vida de un ${pambiInsult} para ser ${pambiInsult}?`; break
+    case 12: phrase = `Dalas recibe acoso porque lo único que ha hecho él en su vida es acosar. Seguid bailandole el agua, ${pambiInsult}s`; break
+    case 13: phrase = `Los ${pambiInsult}s son peor que el coronavirus.`; break
+    case 14: phrase = `Prefiero tener coronavirus a ser ${pambiInsult}`; break
+    default: phrase = `Dalas da mucho asko, pero los ${pambiInsult}s dan aún más askete.`
   }
   return phrase
 }
