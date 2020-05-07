ad956affa3c144b45d775873f7f4d5158336f20e
Hyde_Wick <hydekyle@hotmail.com>
2020-05-07 01:47:45 +0100

    spotify offer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import LandingPage from './components/landing-page/LandingPage'
-import Menu from './components/menu/Menu'
-import ItemCard from './components/item-card/ItemCard'
-import BoxInput from './components/box-input/BoxInput'
-import BannerWelcome from './components/banner-welcome/BannerWelcome'
-import GameMoon from './components/games/game-moons/GameMoons'
-import GamePuzzle from './components/games/game-puzzle/GamePuzzle'
+import OfferSpotify from './components/offer-spotify/OfferSpotify'
 import { MainProvider } from './Context/ContextMain'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
@@ -15,15 +10,6 @@ export default () =>
   <div>
     <MainProvider>
       <Switch>
-
-        <Route path='/shop' component={ItemCard}></Route>
-        <Route path='/moons' component={GameMoon}></Route>
-        <Route path='/puzzle' component={GamePuzzle}></Route>
-        <Route path='/login'>
-          <Menu />
-          <BoxInput />
-          <BannerWelcome />
-        </Route>
         <Route path='/' component={LandingPage}></Route>
       </Switch>
     </MainProvider>
--- src/components/offer-spotify/OfferSpotify.tsx
+++ src/components/offer-spotify/OfferSpotify.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import './LandingPage.css'
+import './OfferSpotify.css'
 import { Button } from 'reactstrap';
 import SpotifyLogo from '../../img/SpotifyIcon.png'
 
@@ -8,16 +8,15 @@ export default () => {
         <div className="page">
             <div className="main-content">
                 <div className="title">
-                    <h1>YEAR OF PREMIUM SPOTIFY!</h1>
+                    <h1>YEAR OF PREMIUM SPOTIFY</h1>
                 </div>
                 <div className="page-description">
-                    <h2>Get ONE year of spotify premium for free!</h2>
+                    Get ONE year of spotify premium for free!
                 </div>
                 <img className="logo" src={SpotifyLogo} alt="" height="200em" width="200em" />
                 <div className="btn-entrar">
                     <Button
                         color="success"
-                        size="lg"
                         onClick={() => window.location.href = 'http://lnkclik.com/8R1o'}
                     >Get premium!
                     </Button>
