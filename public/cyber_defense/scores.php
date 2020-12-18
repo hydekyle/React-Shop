00967f8b1322e3d5d18ef7945e00466c4204c036
Hyde_Wick <hydekyle@hotmail.com>
2020-12-18 05:57:40 +0000

    wp

---
+++ public/cyber_defense/scores.php
@@ -0,0 +1,28 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    define('DB_NAME', 'palolto_spgame');
+    define('DB_USER', 'mktlogic_game_ad');
+    define('DB_PASSWORD', 'eM93bn#6');
+    define('DB_HOST', 'localhost');
+
+    $table_name = "registro";
+    $column_alias = "reg_alias";
+    $column_score = "reg_score";
+    $column_avatar = "reg_avatar";
+    
+    
+    $get_scores = "SELECT * FROM $table_name ORDER BY $column_score DESC LIMIT 9";
+
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    if ($mysqli->connect_errno) exit ("Error de conexión");
+
+    if (!$resultado = $mysqli->query($get_scores)) die ("Error de consulta");
+
+    if ($resultado->num_rows > 0) {
+        while ($fila = $resultado->fetch_array()) {
+            echo "$fila[$column_alias]·$fila[$column_score]·$fila[$column_avatar]|";
+        }
+    }
+    
+?>
