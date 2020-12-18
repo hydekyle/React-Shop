786f1ad4b56e71d34c6ac2bb4a63823a3ee45e84
Hyde_Wick <hydekyle@hotmail.com>
2020-12-18 18:02:42 +0000

    wp

--- public/cyber_defense/textos.php
+++ public/cyber_defense/textos.php
@@ -1,58 +1,58 @@
 <?php
     $historia = 
     "
-<i>2 0 2 1</i>
+2 0 2 1
 
-El año en que las organizaciones están intentando ser vulneradas como nunca por las fuerzas oscuras y potencialmente destructivas del <i>malware</i>. 
+El año en que las organizaciones están intentando ser vulneradas como nunca por las fuerzas oscuras y potencialmente destructivas del malware. 
  
-Hoy, gracias a sus sistemas de vigilancia de última generación, <b>Palo Alto Networks</b> ha podido detectar un intento de intromisión en curso por el <b>Sector Digital</b>, y anticipando el ataque de los <i>Cyber Invaders</i>, se está convocando a los mejores <i>Code-Pilots</i> a formar parte del <i>Cyber Defense Squad</i>.
+Hoy, gracias a sus sistemas de vigilancia de última generación, Palo Alto Networks ha podido detectar un intento de intromisión en curso por el Sector Digital, y anticipando el ataque de los Cyber Invaders, se está convocando a los mejores Code-Pilots a formar parte del Cyber Defense Squad.
 
 Si estás seguro de tener las habilidades y capacidades para ser parte de nuestro escuadrón…
  
-¿Qué esperas para enlistarte, subir a tu nave, y <b>destruir</b> a todo el <i>malware</i> en el <i>ciberespacio</i>?
+¿Qué esperas para enlistarte, subir a tu nave, y destruir a todo el malware en el ciberespacio?
     ";
     
     $nivel1 = 
     "
-Para lograr esta batalla cuentas con <b>3 vidas</b> para completar los <i>5 Cyber Levels</i>.
-Además, contarás con <i>Power Ups</i> que te ayudarán a vencer más fácil a las naves enemigas.
+Para lograr esta batalla cuentas con 3 vidas para completar los 5 Cyber Levels.
+Además, contarás con Power Ups que te ayudarán a vencer más fácil a las naves enemigas.
  
-En este primer nivel, tu nave es una <i>X-Basic Prototype</i>, por lo cual, deberás poner tus capacidades y sentidos al máximo para detener a los <i>Cyber Invaders</i> que vienen de avanzada.
-¡No olvides recargar tus <i>Power Ups</i>!
+En este primer nivel, tu nave es una X-Basic Prototype, por lo cual, deberás poner tus capacidades y sentidos al máximo para detener a los Cyber Invaders que vienen de avanzada.
+¡No olvides recargar tus Power Ups!
     ";
 
     $nivel2 = 
     "
-¡Lo has hecho muy bien!, y por eso, para este nivel, te hemos designado una de nuestras mejores naves; la <i>TX-2020</i>, equipada con mayor velocidad de movimiento y un arma ultra letal y de rápidas descargas.
-Recuerda: un buen <i>Code Pilot</i> nunca se confía. ¡Mantente alerta!, pues en este nivel, las naves del <i>malware</i> serán más difíciles de vencer.
+¡Lo has hecho muy bien!, y por eso, para este nivel, te hemos designado una de nuestras mejores naves; la TX-2020, equipada con mayor velocidad de movimiento y un arma ultra letal y de rápidas descargas.
+Recuerda: un buen Code Pilot nunca se confía. ¡Mantente alerta!, pues en este nivel, las naves del malware serán más difíciles de vencer.
     ";
 
     $nivel3 = 
     "
-Sin duda, eres uno de nuestros mejores pilotos, por eso, tu siguiente misión es el <i>Cyber Level 3</i>, una difícil prueba a tu fuerza interior, pues tendrás que estar atento a un nuevo patrón de ataque y blindaje de las oscuras fuerzas del mal.
-Recuerda recargar tus <i>Power Ups</i>.
+Sin duda, eres uno de nuestros mejores pilotos, por eso, tu siguiente misión es el Cyber Level 3, una difícil prueba a tu fuerza interior, pues tendrás que estar atento a un nuevo patrón de ataque y blindaje de las oscuras fuerzas del mal.
+Recuerda recargar tus Power Ups.
     ";
 
     $nivel4 =
     "
-Por haber llegado a hasta este nivel, recibirás el rango de <i>Code Captain</i>, porque ahora, te enfrentarás a un escuadrón de <i>Cyber Invaders</i> más inteligentes y organizados, por lo cual, la intuición y la velocidad serán tu mejor arma.
+Por haber llegado a hasta este nivel, recibirás el rango de Code Captain, porque ahora, te enfrentarás a un escuadrón de Cyber Invaders más inteligentes y organizados, por lo cual, la intuición y la velocidad serán tu mejor arma.
     ";
 
     $nivel5 = 
     "
-<b>¡Felicidades!</b>
-Lograste llegar al nivel donde solo los <i>Code Pilots</i> más experimentados y con más horas de vuelo lo han conseguido.
+¡Felicidades!
+Lograste llegar al nivel donde solo los Code Pilots más experimentados y con más horas de vuelo lo han conseguido.
  
-Aquí conocerás a los dos principales líderes y archienemigos de la ciberseguridad: <i>Dragon Malware</i>, que si logras vencerlo, podrás ganar una Vida Extra para poder pelear contra el poderoso <i>Lucifer</i>, quien es el <b>Líder Supremo</b> de todo los malwares, si logras eliminarlo, ¡serás todo un guerrero de la Ciberseguridad!
+Aquí conocerás a los dos principales líderes y archienemigos de la ciberseguridad: Dragon Malware, que si logras vencerlo, podrás ganar una Vida Extra para poder pelear contra el poderoso Lucifer, quien es el Líder Supremo de todo los malwares, si logras eliminarlo, ¡serás todo un guerrero de la Ciberseguridad!
     ";
 
     $gameover = 
     "
-<i>Code Pilot</i>, gracias por participar en este Videojuego que hemos diseñado especialmente para ti.
+Code Pilot, gracias por participar en este Videojuego que hemos diseñado especialmente para ti.
 
-En Palo Alto Networks estamos ciertos, que hoy más que nunca, la <b>ciberseguridad</b> debe ser una de las prioridades principales de toda organización, por eso, nuestras soluciones están diseñadas, no solo para contrarrestar ataques cibernéticos, sino también, para prevenirlos. 
+En Palo Alto Networks estamos ciertos, que hoy más que nunca, la ciberseguridad debe ser una de las prioridades principales de toda organización, por eso, nuestras soluciones están diseñadas, no solo para contrarrestar ataques cibernéticos, sino también, para prevenirlos. 
 
-En <b>https://www.paloaltonetworks.com.mx/</b> encontrarás toda la información sobre nuestras soluciones, así como nuestros datos de contacto.
+En https://www.paloaltonetworks.com.mx/ encontrarás toda la información sobre nuestras soluciones, así como nuestros datos de contacto.
 
 ¡Hasta la próxima batalla!
     ";
