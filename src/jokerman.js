b02815a6658dbb86c54023c718d7db6396bd65ea
hydekyle <hydekyle@hotmail.com>
2021-01-11 23:10:29 +0000

    ok

--- src/jokerman.js
+++ src/jokerman.js
@@ -1,46 +1,82 @@
-const robot = require("robotjs")
-const ioHook = require("iohook")
-const fs = require("fs")
+const robot = require("robotjs");
+const ioHook = require("iohook");
+const fs = require("fs");
+const player = require("node-wav-player");
 
 // Config
-let recordMouse = true
+let active = true;
+let recordMouse = true;
 
-let mouseHistory = []
+let mouseHistory = [];
 
-ioHook.on("mousemove", mouseEvent => {
-    if (recordMouse) recordMouseHistory(mouseEvent)
-    //console.log(event);
+ioHook.on("keydown", (event) => {
+  if (active) {
+    if (event.rawcode === 52) shacoUltimateAlarm();
+  }
+
+  if (event.rawcode === 112) switchActiveHacks();
+
+  // if (event.rawcode === 82) saveMouseHistory(mouseHistory)
+  // if (event.rawcode === 70) editAndShot()
+  //console.log(event.rawcode);
 });
 
-ioHook.on("keydown", event => {
-    if (event.rawcode === 82) saveMouseHistory(mouseHistory)
-    if (event.rawcode === 70) editAndShot()
-    //console.log(event)
-})
+// ioHook.on("mousemove", mouseEvent => {
+//     if (recordMouse) recordMouseHistory(mouseEvent)
+//     //console.log(event);
+// });
 
-const recordMouseHistory = mouseEvent => {
-    mouseHistory.push(mouseEvent)
-}
+const switchActiveHacks = () => {
+  active = !active;
+  console.log("Active is now: " + active);
+};
 
-const saveMouseHistory = mouseHistory => {
-    recordMouse = false;
-    loadMouseMovementList(mouseHistory)
-    fs.writeFile("./record.txt", JSON.stringify(mouseHistory), err => {
-        if (err === null) console.log("File written")
-        recordMouse = true;
-    })
-}
+let isUltimateOnCD = false;
 
-const loadMouseMovementList = mouseList => {
-    let clicks = 0
-    mouseList.forEach(mouseValue => {
-        if (mouseValue.clicks > clicks) {
-            robot.mouseClick()
-            clicks++
-        }
-        robot.dragMouse(mouseValue.x, mouseValue.y)
-    })
-}
+const shacoUltimateAlarm = () => {
+  if (isUltimateOnCD) return;
+  isUltimateOnCD = true;
+  setTimeout(() => {
+    player.play({
+      path: "./src/beep.wav",
+    });
+    setTimeout(() => {
+      player.play({
+        path: "./src/beep.wav",
+      });
+      setTimeout(() => {
+        player.play({
+          path: "./src/beep.wav",
+        });
+        isUltimateOnCD = false;
+      }, 1000);
+    }, 1000);
+  }, 15600);
+};
+
+const recordMouseHistory = (mouseEvent) => {
+  mouseHistory.push(mouseEvent);
+};
+
+const saveMouseHistory = (mouseHistory) => {
+  recordMouse = false;
+  loadMouseMovementList(mouseHistory);
+  fs.writeFile("./record.txt", JSON.stringify(mouseHistory), (err) => {
+    if (err === null) console.log("File written");
+    recordMouse = true;
+  });
+};
+
+const loadMouseMovementList = (mouseList) => {
+  let clicks = 0;
+  mouseList.forEach((mouseValue) => {
+    if (mouseValue.clicks > clicks) {
+      robot.mouseClick();
+      clicks++;
+    }
+    robot.dragMouse(mouseValue.x, mouseValue.y);
+  });
+};
 
 // Robotrrr
 
@@ -48,18 +84,18 @@ robot.setMouseDelay(1);
 robot.setKeyboardDelay(1);
 
 var screenSize = robot.getScreenSize();
-var height = (screenSize.height / 2) - 10;
+var height = screenSize.height / 2 - 10;
 var width = screenSize.width;
 
 const editAndShot = () => {
-    var pos = robot.getMousePos()
-    robot.keyTap("z")
-    robot.mouseClick("right")
-    robot.mouseToggle("down")
-    robot.dragMouse(pos.x + 1, pos.y + 1)
-    robot.mouseToggle("up")
-    robot.dragMouse(pos.x, pos.y)
-    robot.keyTap("2")
-}
+  var pos = robot.getMousePos();
+  robot.keyTap("z");
+  robot.mouseClick("right");
+  robot.mouseToggle("down");
+  robot.dragMouse(pos.x + 1, pos.y + 1);
+  robot.mouseToggle("up");
+  robot.dragMouse(pos.x, pos.y);
+  robot.keyTap("2");
+};
 
-ioHook.start()
+ioHook.start();
